0ed990cbeb1cd39a373b397d99a83e1b0805a627
ChannelPixel <ChannelPixel@users.noreply.github.com>
2019-12-08 23:02:48 +0800

    Commit up to start of Lesson 12
    Learned about constructors for derived classes and use cases for const+statics for lookup ids.
    
    Reminds me of RDBMSs

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -12,6 +12,9 @@ namespace Engine
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
 
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+
         public Player(int gold, int experiencePoints, int level,
             int currentHitPoints, int maximumHitPoints)
             : base(currentHitPoints, maximumHitPoints)
@@ -19,6 +22,8 @@ namespace Engine
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
         }
     }
 }
