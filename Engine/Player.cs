4e2f687b508bbdf0736b9fe80a7795d03069c7c6
ChannelPixel <ChannelPixel@users.noreply.github.com>
2019-12-14 13:43:13 +0800

    Append to lesson 19.3 & 19.4
    = Player.cs
     - CreatePlayerFromXmlString() - Error converting inventoryitems in foreach node
     - HasAllQuestCompletionItems() - Complex lambda function didn't work  :(

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -42,55 +42,6 @@ namespace Engine
             return player;
         }
 
-        public static Player CreatePlayerFromXmlString(string xmlPlayerData)
-        {
-            try
-            {
-                XmlDocument playerData = new XmlDocument();
-
-                playerData.LoadXml(xmlPlayerData);
-
-                int currentHitPoints = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/CurrentHitPoints").InnerText);
-                int maximumHitPoints = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/MaximumHitPoints").InnerText);
-                int gold = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/Gold").InnerText);
-                int experiencePoints = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/ExperiencePoints").InnerText);
-
-                Player player = new Player(currentHitPoints, maximumHitPoints, gold, experiencePoints);
-
-                int currentLocationID = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/CurrentLocation").InnerText);
-                player.CurrentLocation = World.LocationByID(currentLocationID);
-
-                foreach(XmlNode node in playerData.SelectNodes("/Player/Stats/InventoryItem"))
-                {
-                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
-                    int quantity = Convert.ToInt32(node.Attributes["Quantity"].Value);
-
-                    for(int i = 0; i < quantity; i++)
-                    {
-                        player.AddItemToInventory(World.ItemByID(id));
-                    }
-                }
-
-
-                foreach (XmlNode node in playerData.SelectNodes("/Player/PlayerQuests/PlayerQuest"))
-                {
-                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
-                    bool isCompleted = Convert.ToBoolean(node.Attributes["IsCompleted"].Value);
-
-                    PlayerQuest playerQuest = new PlayerQuest(World.QuestByID(id));
-                    playerQuest.IsCompleted = isCompleted;
-
-                    player.Quests.Add(playerQuest);
-                }
-
-                return player;
-            }
-            catch
-            {
-                return Player.CreateDefaultPlayer();
-            }
-        }
-
         public bool HasRequiredItemToEnterThisLocation(Location location)
         {
             if (location.ItemRequiredToEnter == null)
@@ -107,38 +58,35 @@ namespace Engine
         {
             //LAMBDA LINQ FOREACH substitute
             //Lession 19.3
-            return Quests.Exists(ii => ii.Details.ID == quest.ID);
+            return Quests.Exists(pq => pq.Details.ID == quest.ID);
         }
 
         public bool CompletedThisQuest(Quest quest)
         {
-            return Quests.Exists(pq => pq.Details.ID == quest.ID);
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if (playerQuest.Details.ID == quest.ID)
+                {
+                    return playerQuest.IsCompleted;
+                }
+            }
 
+            return false;
         }
 
         public bool HasAllQuestCompletionItems(Quest quest)
         {
             foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
             {
-                if (Inventory.Exists(ii => ii.Details.ID == qci.Details.ID
-                                            && ii.Quantity < qci.Quantity))
+                // Check each item in the player's inventory, to see if they have it, and enough of it
+                if (!Inventory.Exists(ii => ii.Details.ID == qci.Details.ID && ii.Quantity >= qci.Quantity))
                 {
                     return false;
                 }
             }
+
+            // If we got here, then the player must have all the required items, and enough of them, to complete the quest.
             return true;
-            
-
-            /*/19.3 Complex lambda expression with conditional
-            return quest.QuestCompletionItems.Exists(qci => {
-
-                if(Inventory.Exists(ii => ii.Details.ID == qci.Details.ID && ii.Quantity < qci.Quantity))
-                {
-                    return false;
-                }
-
-                return true;
-            });*/
         }
 
         public void RemoveQuestCompletionItems(Quest quest)
@@ -252,6 +200,55 @@ namespace Engine
             }
 
             return playerData.InnerXml;
+        }
+
+        public static Player CreatePlayerFromXmlString(string xmlPlayerData)
+        {
+            try
+            {
+                XmlDocument playerData = new XmlDocument();
+
+                playerData.LoadXml(xmlPlayerData);
+
+                int currentHitPoints = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/CurrentHitPoints").InnerText);
+                int maximumHitPoints = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/MaximumHitPoints").InnerText);
+                int gold = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/Gold").InnerText);
+                int experiencePoints = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/ExperiencePoints").InnerText);
+
+                Player player = new Player(currentHitPoints, maximumHitPoints, gold, experiencePoints);
+
+                int currentLocationID = Convert.ToInt32(playerData.SelectSingleNode("Player/Stats/CurrentLocation").InnerText);
+                player.CurrentLocation = World.LocationByID(currentLocationID);
+
+                foreach (XmlNode node in playerData.SelectNodes("/Player/InventoryItems/InventoryItem"))
+                {
+                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
+                    int quantity = Convert.ToInt32(node.Attributes["Quantity"].Value);
+
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        player.AddItemToInventory(World.ItemByID(id));
+                    }
+                }
+
+
+                foreach (XmlNode node in playerData.SelectNodes("/Player/PlayerQuests/PlayerQuest"))
+                {
+                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
+                    bool isCompleted = Convert.ToBoolean(node.Attributes["IsCompleted"].Value);
+
+                    PlayerQuest playerQuest = new PlayerQuest(World.QuestByID(id));
+                    playerQuest.IsCompleted = isCompleted;
+
+                    player.Quests.Add(playerQuest);
+                }
+
+                return player;
+            }
+            catch
+            {
+                return Player.CreateDefaultPlayer();
+            }
         }
     }
 }
